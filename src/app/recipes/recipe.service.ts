cad2b4c66c0d0e9d39e23579978a10888ae2815a
sree318 <sabharisree99@gmail.com>
2024-05-18 19:22:06 +0530

    http Request

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,31 +6,32 @@ import { Recipe } from './recipe.model';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Briyani',
-      'Spicy and tasty',
-      'https://img.freepik.com/free-photo/gourmet-chicken-biryani-with-steamed-basmati-rice-generated-by-ai_188544-13480.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('Rice', 2)]
-    ),
-    new Recipe(
-      ' Fried Rice',
-      'Colorful and Yummy',
-      'https://kfoods.com/images1/newrecipeicon/chinese-egg-fried-rice_1418.jpg',
-      [
-        new Ingredient('Carrot', 1),
-        new Ingredient('Beans', 2),
-        new Ingredient('Rice', 1),
-        new Ingredient('Cabbage', 1),
-      ]
-    ),
-    new Recipe(
-      'Lemon Rice',
-      'Rich in taste',
-      'https://img.global.news.samsung.com/in/wp-content/uploads/2021/06/ccc.jpg',
-      [new Ingredient('Lemon', 1), new Ingredient('Rice', 1 / 2)]
-    ),
-  ];
+  private recipes: Recipe[] = [];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Briyani',
+  //     'Spicy and tasty',
+  //     'https://img.freepik.com/free-photo/gourmet-chicken-biryani-with-steamed-basmati-rice-generated-by-ai_188544-13480.jpg',
+  //     [new Ingredient('Meat', 1), new Ingredient('Rice', 2)]
+  //   ),
+  //   new Recipe(
+  //     ' Fried Rice',
+  //     'Colorful and Yummy',
+  //     'https://kfoods.com/images1/newrecipeicon/chinese-egg-fried-rice_1418.jpg',
+  //     [
+  //       new Ingredient('Carrot', 1),
+  //       new Ingredient('Beans', 2),
+  //       new Ingredient('Rice', 1),
+  //       new Ingredient('Cabbage', 1),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Lemon Rice',
+  //     'Rich in taste',
+  //     'https://img.global.news.samsung.com/in/wp-content/uploads/2021/06/ccc.jpg',
+  //     [new Ingredient('Lemon', 1), new Ingredient('Rice', 1 / 2)]
+  //   ),
+  // ];
   constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
@@ -58,6 +59,11 @@ export class RecipeService {
 
   deleteRecipe(index: number) {
     this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
     this.recipesChanged.next(this.recipes.slice());
   }
 }
